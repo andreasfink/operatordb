6c17aa75eda388b0316ba5029ccc5c4b63a5ceb9
Andreas Fink <andreas@fink.org>
2020-03-05 16:50:16 +0100

    make framework for osx

--- make_framework.sh
+++ make_framework.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
-FRAMEWORK_DIR=/Library/Frameworks/operatordb.framework
+FRAMEWORK_DIR=/Library/Application\ Support/FinkTelecomServices/frameworks/operatordb.framework/
 
-#rm -rf "${FRAMEWORK_DIR}"
+
+install_name_tool -id  "${FRAMEWORK_DIR}/Versions/A/operatordb"  liboperatordb.dylib
+/usr/bin/codesign --force --sign 8FF801F4427AE4DFB624CC8D8D41EFD33E8A1BBD -o runtime --timestamp=none liboperatordb.dylib
+rm -rf "${FRAMEWORK_DIR}"
 mkdir -p "${FRAMEWORK_DIR}/Versions/A/Headers"
 mkdir -p "${FRAMEWORK_DIR}/Versions/A/Resources"
 cp Info.plist "${FRAMEWORK_DIR}/Versions/A/Resources/Info.plist"
@@ -10,11 +13,14 @@ cp VERSION "${FRAMEWORK_DIR}/Versions/A/Resources/VERSION"
 cp operatordb.h "${FRAMEWORK_DIR}/Versions/A/Headers/operatordb.h"
 cp liboperatordb.dylib "${FRAMEWORK_DIR}/Versions/A/operatordb"
 
-cd "${FRAMEWORK_DIR}"
+pushd "${FRAMEWORK_DIR}"
 ln -s Versions/Current/Headers Headers
 ln -s Versions/Current/Resources Resources
 ln -s Versions/Current/operatordb operatordb
 
 cd "${FRAMEWORK_DIR}/Versions"
 ln -s A Current
-cd ..
+popd
+
+
+
